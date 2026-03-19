f4d9fb2a4b273d3b779d0a1725e2a860693cff78
santhoshkumar <priy.santhosh1992@gmail.com>
2026-03-19 21:39:42 +0530

    sadfasdfas

--- src/test/java/basepackage/BaseClass.java
+++ src/test/java/basepackage/BaseClass.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -23,8 +24,14 @@ public class BaseClass {
 	protected static WebDriverWait wait;
 
 	public void init() throws IOException {
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless"); // Run without GUI
+		options.addArguments("--disable-gpu");
+		options.addArguments("--window-size=1920,1080");
+		options.addArguments("--no-sandbox"); // required on some CI environments
+		options.addArguments("--disable-dev-shm-usage"); // for memory issues in CI
 		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
+		driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
 		ConfigReader.initPropertyFile();
 		driver.get(ConfigReader.getValue("url"));
